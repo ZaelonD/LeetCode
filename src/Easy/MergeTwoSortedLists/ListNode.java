2612f6f3c946a2abed16fc825bdddfa0ad65d2d7
Alexander Dubrovin <xah799@mail.ru>
2023-04-13 23:31:44 +0300

    Initial commit

--- src/Easy/MergeTwoSortedLists/ListNode.java
+++ src/Easy/MergeTwoSortedLists/ListNode.java
@@ -3,6 +3,11 @@ package Easy.MergeTwoSortedLists;
 //Input: list1 = [1,2,4], list2 = [1,3,4]
 //        Output: [1,1,2,3,4,4]
 
+//Input: list1 = [], list2 = []
+//        Output: []
+
+//Input: list1 = [], list2 = [0]
+//        Output: [0]
 
 public class ListNode {
     int val;
