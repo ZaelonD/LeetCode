25ca86ac31c85e600afb6740e9c360fd51793f0c
Александр Дубровин <xah799@mail.ru>
2023-08-03 17:29:52 +0300

    Created removeDuplicates() method. Runtime - 1 ms. Memory - 43.78 mb.

--- src/Easy/RemoveDuplicatesFromSortedArray/Remove.java
+++ src/Easy/RemoveDuplicatesFromSortedArray/Remove.java
@@ -1,22 +1,16 @@
 package Easy.RemoveDuplicatesFromSortedArray;
 
-import java.util.HashSet;
-
 public class Remove {
     public static int removeDuplicates(int[] nums) {
-        int[] temp = new int[nums.length];
-        int j = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] != nums[i + 1]) {
-                temp[j++] = nums[i];
+        int uniqueElement = nums[0];
+        int uniqueElementCount = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != uniqueElement) {
+                nums[uniqueElementCount] = nums[i]; //sets new number to next index after unique element
+                uniqueElement = nums[i]; // set unique element to new number
+                uniqueElementCount++; // updates element count and also used for indexing
             }
         }
-        temp[j] = nums[nums.length - 1];
-        System.arraycopy(temp, 0, nums, 0, nums.length);
-        HashSet<Integer> hashSet = new HashSet<>();
-        for (int i = 0; i <= j; i++) {
-            hashSet.add(nums[i]);
-        }
-        return hashSet.size();
+        return uniqueElementCount;
     }
 }
