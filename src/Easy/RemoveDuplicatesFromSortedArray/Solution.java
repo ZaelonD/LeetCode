5220db8829047a9b8ef6115ef5da1d46a8e92a31
Александр Дубровин <xah799@mail.ru>
2023-08-03 17:18:45 +0300

    Created removeDuplicates() method. Runtime - 1 ms. Memory - 44.41 mb.

--- src/Easy/RemoveDuplicatesFromSortedArray/Solution.java
+++ src/Easy/RemoveDuplicatesFromSortedArray/Solution.java
@@ -1,4 +1,13 @@
 package Easy.RemoveDuplicatesFromSortedArray;
 
 public class Solution {
+    public static void main(String[] args) {
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        int[] expectedNums = {0, 1, 2, 3, 4};
+        int k = Remove.removeDuplicates(nums); // Calls your implementation
+        assert k == expectedNums.length;
+        for (int i = 0; i < k; i++) {
+            assert nums[i] == expectedNums[i];
+        }
+    }
 }
