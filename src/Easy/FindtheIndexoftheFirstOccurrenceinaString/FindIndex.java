6a3ce3b1e640eba2d4a2b025ba1eb5e7c896bd59
Александр Дубровин <xah799@mail.ru>
2023-08-21 16:35:43 +0300

    Created strStr() method. Runtime - 0 ms. Memory - 40.1 mb.

---
+++ src/Easy/FindtheIndexoftheFirstOccurrenceinaString/FindIndex.java
@@ -0,0 +1,7 @@
+package Easy.FindtheIndexoftheFirstOccurrenceinaString;
+
+public class FindIndex {
+    public static int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
---
+++ src/Easy/FindtheIndexoftheFirstOccurrenceinaString/Solution.java
@@ -0,0 +1,7 @@
+package Easy.FindtheIndexoftheFirstOccurrenceinaString;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.print(FindIndex.strStr("leetcode", "leeto"));
+    }
+}
